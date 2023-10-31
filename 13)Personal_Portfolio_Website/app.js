a581e64b050516d29133b81fd0114eb74722b4a0
777beybey <beyzaselvi777@gmail.com>
2023-11-01 00:12:55 +0300

    personal portfolio
    this is my personal website 2nd edition, better than the 1st one. Check it out.

---
+++ 13)Personal_Portfolio_Website/app.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text" , {
+    strings:["Full Stack Developer","Youtuber","Artist","Model"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
